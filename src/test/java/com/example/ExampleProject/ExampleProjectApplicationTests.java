bb8a13604add09dc193b9a33938ce9abce2f84f3
ajith1911 <35368794+ajith1911@users.noreply.github.com>
2023-06-14 01:02:51 +0530

    Update ExampleProjectApplicationTests.java

--- src/test/java/com/example/ExampleProject/ExampleProjectApplicationTests.java
+++ src/test/java/com/example/ExampleProject/ExampleProjectApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.ExampleProject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*Upadted*/
 @SpringBootTest
 class ExampleProjectApplicationTests {
 
