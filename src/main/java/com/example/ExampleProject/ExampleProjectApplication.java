e51e005c3744bab9f5365625cafe807481aeaac7
Ajith <ajithkumar1911@gmail.com>
2023-09-27 11:09:07 +0530

    test

--- src/main/java/com/example/ExampleProject/ExampleProjectApplication.java
+++ src/main/java/com/example/ExampleProject/ExampleProjectApplication.java
@@ -2,7 +2,7 @@ package com.example.ExampleProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//added in new-b
 @SpringBootApplication
 public class ExampleProjectApplication {
 
